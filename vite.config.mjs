affe2aa6765f62ce82136ed107291328ad258bbf
JC <jchan48@gmail.com>
2023-03-21 21:49:02 -0400

    updated README.md with gh url

--- vite.config.mjs
+++ vite.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
         // entry: fileURLToPath(new URL('./src/index.mjs', import.meta.url))
         entry:[ //"entry" can be a dictionary or array of multiple entry points
           fileURLToPath(new URL('./src/index.mjs', import.meta.url)),
-          fileURLToPath(new URL('./src/nested_function_static_example.mjs', import.meta.url)),
+          // fileURLToPath(new URL('./src/nested_function_static_example.mjs', import.meta.url)),
         ]
       }
 },
