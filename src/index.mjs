5a90edbdd396000f357c4f9a24dac9add27e814e
JC <jchan48@gmail.com>
2023-06-18 10:18:27 -0400

    updated alias and ran the test

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,4 @@
-// import {hi,bigTreeShakingFile} from "##/example_large_data_js.mjs";
-import {hi,bigTreeShakingFile} from "##cwd/src/example_large_data_js.mjs";
+import {hi,bigTreeShakingFile} from "src/example_large_data_js.mjs";
 export function run(){
   return hi
   // return hi + bigTreeShakingFile
